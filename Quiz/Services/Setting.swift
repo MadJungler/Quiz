8ab98ecd21065a69658540e51f10d0dc19fc0a06
Павел <terminator@MBP-Pavel.Dlink>
2022-10-31 14:06:39 +0300

    HW 10-11

---
+++ Quiz/Services/Setting.swift
@@ -0,0 +1,8 @@
+//
+//  Setting.swift
+//  Quiz
+//
+//  Created by Павел on 30.10.2022.
+//
+
+import Foundation
---
+++ Quiz/SetupViewController/SetupView.swift
@@ -0,0 +1,20 @@
+//
+//  SetupView.swift
+//  Quiz
+//
+//  Created by Павел on 27.10.2022.
+//
+
+import UIKit
+
+class SetupView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
